fef9d2a0fbbe92ba62ba4cfdd9aba42108da1d97
Dmitrii Tereshchenko <dtereshch@gmail.com>
2023-09-06 20:48:08 +0300

    Update number.R

--- R/number.R
+++ R/number.R
@@ -1,3 +1,14 @@
+#' Counting the number of non-missing values
+#'
+#' Created to count the number of non-missing values in a column inside dplyr::summarise()
+#' @param x The column of interest
+#' @return The number of non-missing values in x
+#' @examples 
+#' library(dplyr);
+#' data("cars");
+#' cars %>% summarise(across(everything(), list(n = number, mean = mean, sd = sd), na.rm = TRUE));
+#' cars %>% summarise(across(everything(), \(x) number(x, na.rm = TRUE)));
+#' @export
 number <- function(x, na.rm = TRUE){
   return(sum(!is.na(x)))
   }
