e130b79e398e7d03643982b60d8cf4d78aa5170e
Dmitrii Tereshchenko <dtereshch@gmail.com>
2023-09-09 08:56:32 +0300

    Create describe_panel.R

---
+++ R/describe_panel.R
@@ -0,0 +1,27 @@
+#' Calculating descriptive statistics for selected variables in the panel data
+#'
+#' Inspired by Stata's xtsum command and corresponding discussion in https://stackoverflow.com/questions/49282083/xtsum-command-for-r. 
+#' Calculates the mean, the variance, and the standard deviation of selected variables for panel data. 
+#' Decomposes the variance and the standard deviation into between and within components.
+#' @param data The data frame
+#' @param varnames The vector of numeric variables of interest
+#' @param unit The object id variable
+#' @return The tibble with the mean, the variation, and the standard deviation of selected variables for panel data. 
+#' @examples 
+#' data("Crime", package = "plm")
+#' vars_selection <- c("crmrte", "polpc")
+#' describe_panel(Crime, vars_selection, "county")
+#' @export
+describe_panel <- function(data, varnames, unit) {
+  require(rlang)
+  require(dplyr)
+  
+  desc_table <- tibble()
+  
+  for (varname in varnames) {
+    desc_table <- desc_table %>%
+      bind_rows(describe_panel_1(data, !!sym(varname), !!sym(unit)))
+  }
+  
+  return(desc_table)
+}
