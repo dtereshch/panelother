e59b225aa5346bfcde69afe8d0b33ee585c0ce7a
Dmitrii Tereshchenko <dtereshch@gmail.com>
2023-09-09 08:46:22 +0300

    Renaming functions

--- R/describe.R
+++ R/describe.R
@@ -1,22 +1,19 @@
-#' Calculating group descriptive statistics 
+#' Calculating basic descriptive statistics 
 #'
-#' Created to provide group descriptive statistics in the tibble format
+#' Created to provide basic descriptive statistics in the tibble format
 #' @param data The data frame
 #' @param vars The vector of numeric columns/variables of interest
-#' @param by The vector of factor columns/variables representing groups
-#' @return The tibble with group descriptive statistics of selected columns
+#' @return The tibble with basic descriptive statistics of selected columns
 #' @examples 
 #' data("iris")
 #' vars_select <- c("Sepal.Length", "Sepal.Width")
-#' group_vars <- c("Species")
-#' descby(iris, vars_select, group_vars)
+#' describe(iris, vars_select) 
 #' @export
-descby <- function(data, vars, by){
+describe <- function(data, vars){
   require(dplyr)
   require(tidyr)
-
+  
   desc_tibble <- data %>%
-    group_by(across(all_of(by))) %>%
     select(all_of(vars)) %>%
     summarise(across(everything(), 
                      list(n = \(x) tidyother::number(x), 
@@ -25,11 +22,11 @@ descby <- function(data, vars, by){
                           sd = \(x) sd(x, na.rm = TRUE), 
                           min = \(x) min(x, na.rm = TRUE), 
                           max = \(x) max(x, na.rm = TRUE)))) %>% 
-    pivot_longer(where(is.numeric), names_to = "name", values_to = "value") %>% 
+    pivot_longer(everything(), names_to = "name", values_to = "value") %>% 
     separate(name, c("variable", "statistic"), sep = "_") %>%
     pivot_wider(names_from = statistic, values_from = value) %>%
     arrange(variable) %>% 
-    select(variable, by, n, mean, median, sd, min, max)
+    select(variable, n, mean, median, sd, min, max)
   
   return(desc_tibble)
 }
--- R/describe_by.R
+++ R/describe_by.R
@@ -1,19 +1,22 @@
-#' Calculating basic descriptive statistics 
+#' Calculating group descriptive statistics 
 #'
-#' Created to provide basic descriptive statistics in the tibble format
+#' Created to provide group descriptive statistics in the tibble format
 #' @param data The data frame
 #' @param vars The vector of numeric columns/variables of interest
-#' @return The tibble with basic descriptive statistics of selected columns
+#' @param by The vector of factor columns/variables representing groups
+#' @return The tibble with group descriptive statistics of selected columns
 #' @examples 
 #' data("iris")
 #' vars_select <- c("Sepal.Length", "Sepal.Width")
-#' desc(iris, vars_select) 
+#' group_vars <- c("Species")
+#' describe_by(iris, vars_select, group_vars)
 #' @export
-desc <- function(data, vars){
+describe_by <- function(data, vars, by){
   require(dplyr)
   require(tidyr)
-  
+
   desc_tibble <- data %>%
+    group_by(across(all_of(by))) %>%
     select(all_of(vars)) %>%
     summarise(across(everything(), 
                      list(n = \(x) tidyother::number(x), 
@@ -22,11 +25,11 @@ desc <- function(data, vars){
                           sd = \(x) sd(x, na.rm = TRUE), 
                           min = \(x) min(x, na.rm = TRUE), 
                           max = \(x) max(x, na.rm = TRUE)))) %>% 
-    pivot_longer(everything(), names_to = "name", values_to = "value") %>% 
+    pivot_longer(where(is.numeric), names_to = "name", values_to = "value") %>% 
     separate(name, c("variable", "statistic"), sep = "_") %>%
     pivot_wider(names_from = statistic, values_from = value) %>%
     arrange(variable) %>% 
-    select(variable, n, mean, median, sd, min, max)
+    select(variable, by, n, mean, median, sd, min, max)
   
   return(desc_tibble)
 }
