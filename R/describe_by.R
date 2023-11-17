52b54aa3ceca6690b91e60d6028225579b9cf8f2
Dmitrii Tereshchenko <dtereshch@gmail.com>
2023-11-17 15:12:56 +0300

    Update describe_by

--- R/describe_by.R
+++ R/describe_by.R
@@ -7,16 +7,16 @@
 #' @return The tibble with group descriptive statistics of selected columns
 #' @examples 
 #' data("iris")
-#' vars_select <- c("Sepal.Length", "Sepal.Width")
-#' describe_by(iris, vars_select, "Species")
+#' vars_select <- c(Sepal.Length, Sepal.Width)
+#' describe_by(iris, vars_select, Species)
 #' @export
 describe_by <- function(data, varnames, by){
   require(dplyr)
   require(tidyr)
 
   desc_tibble <- data %>%
-    group_by(!!sym(by)) %>%
-    select(all_of(varnames)) %>%
+    group_by({{ by }}) %>%
+    select({{ varnames }}) %>%
     summarise(across(everything(), 
                      list(n = \(x) tidyother::number(x), 
                           mean = \(x) mean(x, na.rm = TRUE), 
@@ -24,8 +24,10 @@ describe_by <- function(data, varnames, by){
                           sd = \(x) sd(x, na.rm = TRUE), 
                           min = \(x) min(x, na.rm = TRUE), 
                           max = \(x) max(x, na.rm = TRUE)))) %>% 
-    pivot_longer(-!!sym(by), names_to = "name", values_to = "value") %>% 
+    pivot_longer(-{{by}}, names_to = "name", values_to = "value") %>% 
     extract(name, into = c("variable", "statistic"), "(.*)_([^_]+)$") %>%
+    #separate(name, into = c("variable", "statistic"), sep="_(?=[^_]+$)") %>%
+    #separate(name, c("variable", "statistic"), sep = "_") %>%
     pivot_wider(names_from = statistic, values_from = value) %>%
     arrange(variable)
   
