5e8dde7166d0480f8a250ed8fba444d1e3c71b8d
Dmitrii Tereshchenko <dtereshch@gmail.com>
2023-11-17 14:31:03 +0300

    Update describe_by.R

--- R/describe_by.R
+++ R/describe_by.R
@@ -16,7 +16,7 @@ describe_by <- function(data, varnames, by){
   require(stringr)
   
   varnames <- str_replace_all(varnames, "_", ".")
-  data <- rename_all(list(~ stringr::str_replace_all(., "_", ".")))
+  data <- rename_all_(.dots = list(~ stringr::str_replace_all(., "_", ".")))
 
   desc_tibble <- data %>%
     group_by(!!sym(by)) %>%
